67ea825955a4db856f4cfc35cd49f819f543fe88
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2021-02-11 16:11:49 +0700

    Modify FE

--- FE/pages/index.vue
+++ FE/pages/index.vue
@@ -2,28 +2,31 @@
   <div class="container">
     <div class="mt-5">
       <h1 class="mb-3 text-center text-md-left">
-            Bootstrap<span class="text-vue-green">Vue</span> Member</h1>
-      <table class="table">
-        <tr>
-          <th>Fullname</th>
-          <th>Email</th>
-          <th>Password</th>
-          <th class="text-center">
-            <button class="btn btn-sm btn-info" v-b-modal.modal-1>Add Member</button>
-          </th>
-        </tr>
-        <tr v-for="user in datauser">
-          <td>{{user.fullname}}</td>
-          <td>{{user.email}}</td>
-          <td>********</td>
-          <th class="text-center"><button class="btn btn-sm btn-danger" @click="deleteuser(user._id)">Delete</button></th>
-        </tr>
-      </table>
+        Bootstrap<span class="text-vue-green">Vue</span> Member</h1>
+      <div class="table-responsive">
+        <table class="table">
+          <tr>
+            <th>Fullname</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th class="text-center">
+              <button class="btn btn-sm btn-info" v-b-modal.modal-1>Add Member</button>
+            </th>
+          </tr>
+          <tr v-for="user in datauser">
+            <td>{{user.fullname}}</td>
+            <td>{{user.email}}</td>
+            <td>********</td>
+            <th class="text-center"><button class="btn btn-sm btn-danger" @click="deleteuser(user._id)">Delete</button>
+            </th>
+          </tr>
+        </table>
+      </div>
       <b-modal id="modal-1" title="Form Member BootstrapVue" hide-footer="true">
         <b-form-input v-model="fullname" placeholder="Fullname" class="mb-2"></b-form-input>
         <b-form-input v-model="email" placeholder="Email" class="mb-2"></b-form-input>
         <b-form-input v-model="password" placeholder="Password" class="mb-2"></b-form-input>
-        <b-button variant="info" class="btn-block" @click="adduser">Save</b-button>
+        <b-button variant="info" class="btn-block" @click="adduser">Saves</b-button>
       </b-modal>
     </div>
   </div>
