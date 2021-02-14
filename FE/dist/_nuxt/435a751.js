313c2e6d7bf3100d36d5931a28950c08650659cd
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2021-02-14 11:22:08 +0700

    Modify  FE Login

---
+++ FE/dist/_nuxt/435a751.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{274:function(n,t,e){"use strict";e.r(t);var c={},l=e(60),component=Object(l.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container"},[this._v("\n  HELLO\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
--- FE/pages/index.vue
+++ FE/pages/index.vue
@@ -1,86 +1,10 @@
 <template>
   <div class="container">
-    <div class="mt-5">
-      <h1 class="mb-3 text-center text-md-left">
-        Bootstrap<span class="text-vue-green">Vue</span> Member</h1>
-      <div class="table-responsive">
-        <table class="table">
-          <tr>
-            <th>Fullname</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th class="text-center">
-              <button class="btn btn-sm btn-info" v-b-modal.modal-1>Add Member</button>
-            </th>
-          </tr>
-          <tr v-for="user in datauser">
-            <td>{{user.fullname}}</td>
-            <td>{{user.email}}</td>
-            <td>********</td>
-            <th class="text-center"><button class="btn btn-sm btn-danger" @click="deleteuser(user._id)">Delete</button>
-            </th>
-          </tr>
-        </table>
-      </div>
-      <b-modal id="modal-1" title="Form Member BootstrapVue" hide-footer="true">
-        <b-form-input v-model="fullname" placeholder="Fullname" class="mb-2"></b-form-input>
-        <b-form-input v-model="email" placeholder="Email" class="mb-2"></b-form-input>
-        <b-form-input v-model="password" placeholder="Password" class="mb-2"></b-form-input>
-        <b-button variant="info" class="btn-block" @click="adduser">Saves</b-button>
-      </b-modal>
-    </div>
+    HELLO
   </div>
 </template>
 
 <script>
-  var axios = require('axios');
   export default {
-    async fetch() {
-      var dataitem = {
-        method: 'get',
-        url: 'http://147.139.181.78:3000/member'
-      };
-      axios(dataitem)
-        .then(res => {
-          this.datauser = res.data;
-        })
-    },
-    fetchOnServer: false,
-    data() {
-      return {
-        fullname: '',
-        email: '',
-        password: '',
-        datauser: []
-      }
-    },
-    methods: {
-      deleteuser(id) {
-        var dataitem = {
-          method: 'delete',
-          url: 'http://147.139.181.78:3000/member/' + id
-        };
-        axios(dataitem)
-          .then(res => {
-            this.$fetch()
-          })
-      },
-      adduser() {
-        var data = JSON.stringify({ "fullname": this.fullname, "email": this.email, "password": this.password });
-        var dataitem = {
-          method: 'post',
-          url: 'http://147.139.181.78:3000/member',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: data
-        };
-        axios(dataitem)
-          .then(res => {
-            this.$bvModal.hide('modal-1');
-            this.$fetch()
-          })
-      }
-    },
   }
 </script>
